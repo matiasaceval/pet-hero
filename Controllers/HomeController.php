ac9242b1d4a01fe85b0a1f5a1e1c487a4f642eb1
Matias Aceval <matiasacevalventura@gmail.com>
2022-10-18 00:47:15 -0300

    🆕 add: base framework

---
+++ Controllers/HomeController.php
@@ -0,0 +1,9 @@
+<?php
+    namespace Controllers;
+
+    class HomeController {
+        public function Index() {
+            require_once(VIEWS_PATH . "index.php");
+        }        
+    }
+?>
---
+++ Views/header.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH ?>bootstrap.min.css">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="<?php echo VIEWS_PATH ?>img/favicon.ico" type="image/x-icon" />
+    <title>Pet Hero</title>
+</head>
+<body>
