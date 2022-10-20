b5ff7cbcfbd691cf65e5d22e0d2a492cdb7291da
joaquin-labrador <joaquinlabrador1010@gmail.com>
2022-10-20 20:26:26 -0300

    ♻ update(DAO): complete all methods

--- DAO/PetDAOJson.php
+++ DAO/PetDAOJson.php
@@ -3,6 +3,7 @@
 namespace DAO;
 
 use Models\Pet;
+use DAO\OwnerDAOJson as OwnerDAO;
 
 class PetDAOJson implements IPetDAO
 {
@@ -18,35 +19,133 @@ class PetDAOJson implements IPetDAO
         $this->fileName = ROOT . "/Data/pets.json";
     }
 
-    private function RetrieveData(){}
+    private function RetrieveData()
+    {
+        $this->petList = array();
+
+        if (file_exists($this->fileName)) {
+            $jsonContent = file_get_contents($this->fileName);
+
+            $arrayToDecode = ($jsonContent) ? json_decode($jsonContent, true) : array();
+
+            foreach ($arrayToDecode as $valuesArray) {
+                $pet = new Pet();
+                $pet->setId($valuesArray["id"]);
+                $pet->setName($valuesArray["name"]);
+                $pet->setAge($valuesArray["age"]);
+                $pet->setSpecies($valuesArray["species"]);
+                $pet->setBreed($valuesArray["breed"]);
+                array_push($this->petList, $pet);
+            }
+        }
+
+    }
+
+    private function SaveData()
+    {
+        $arrayToEncode = array();
+        /**
+         * @var Pet $pet
+         */
+        foreach ($this->petList as $pet) {
+            $valuesArray["id"] = $pet->getId();
+            $valuesArray["name"] = $pet->getName();
+            $valuesArray["age"] = $pet->getAge();
+            $valuesArray["species"] = $pet->getSpecies();
+            $valuesArray["breed"] = $pet->getBreed();
+            $valuesArray["ownerId"] = $pet->getOwner()->getId();
+            array_push($arrayToEncode, $valuesArray);
+        }
+
+        $jsonContent = json_encode($arrayToEncode, JSON_PRETTY_PRINT);
+
+        file_put_contents($this->fileName, $jsonContent);
+    }
+
+    private function GetNextId()
+    {
+        $id = 0;
+
+        foreach ($this->petList as $pet) {
+            $id = ($pet->getId() > $id) ? $pet->getId() : $id;
+        }
+
+        return $id + 1;
+    }
 
     public function Add(Pet $pet)
     {
-        // TODO: Implement Add() method.
+        $this->RetrieveData();
+
+        $pet->setId($this->GetNextId());
+
+        array_push($this->petList, $pet);
+
+        $this->SaveData();
     }
 
-    public function GetAll(): array
+    public function &GetAll(): array
     {
-        // TODO: Implement GetAll() method.
+        $this->RetrieveData();
+
+        return $this->petList;
     }
 
     public function GetById(int $id): ?Pet
     {
-        // TODO: Implement GetById() method.
+        $this->RetrieveData();
+
+        $owner = array_filter($this->petList, fn($pet) => $pet->getId() == $id);
+
+        return array_shift($owner);
     }
 
     public function RemoveById(int $id): bool
     {
-        // TODO: Implement RemoveById() method.
+        $this->RetrieveData();
+
+        $newList = array_filter($this->petList, fn($pet) => $pet->getId() != $id);
+
+        $bool = count($newList) < count($this->petList);
+
+        $this->petList = $newList;
+
+        $this->SaveData();
+
+        return $bool;
+
     }
 
     public function Update(Pet $pet): bool
     {
-        // TODO: Implement Update() method.
+       $this->RetrieveData();
+
+       foreach ($this->petList as $key => $value) {
+           if($value->getId() == $pet->getId()){
+               $this->petList[$key] = $pet;
+               $this->SaveData();
+               return true;
+           }
+       }
+
     }
 
-    public function GetByOwnerId(int $ownerId): array
+    public function GetOwnerId(int $petId): ?int
     {
-        // TODO: Implement GetByOwnerId() method.
+        $this->petList = array();
+
+        if (file_exists($this->fileName)) {
+            $jsonContent = file_get_contents($this->fileName);
+
+            $arrayToDecode = ($jsonContent) ? json_decode($jsonContent, true) : array();
+
+            foreach ($arrayToDecode as $valuesArray) {
+                if ($valuesArray["id"] == $petId) {
+                    return $valuesArray["ownerId"];
+                }
+            }
+            return null;
+        }
+
     }
 }
