4e95c500ed8b3638cf640ac1439f970f49a894b4
joaquin-labrador <joaquinlabrador1010@gmail.com>
2022-10-21 15:17:46 -0300

    ✨  feat(controller): the owner could list the keepers

--- Controllers/OwnerController.php
+++ Controllers/OwnerController.php
@@ -4,20 +4,27 @@ namespace Controllers;
 
 use DAO\OwnerDAOJson as OwnerDAO;
 use DAO\PetDAOJson as PetDAO;
+use DAO\KeeperDAOJson as KeeperDAO;
 use Models\Owner as Owner;
 use Models\Pet;
+use Models\Keeper as Keeper;
 use Utils\Session;
 
-class OwnerController {
+class OwnerController
+{
     private OwnerDAO $ownerDAO;
     private PetDAO $petDAO;
+    private KeeperDAO $keeperDAO;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->ownerDAO = new OwnerDAO();
         $this->petDAO = new PetDAO();
+        $this->keeperDAO = new KeeperDAO();
     }
 
-    public function Index() {
+    public function Index()
+    {
         $this->VerifyIsLogged();
 
         $owner = Session::Get("owner");
@@ -25,20 +32,21 @@ class OwnerController {
         // TODO: Owner home view
     }
 
-    public function SignUp(string $firstname, string $lastname, string $email, string $phone, string $password, string $confirmPassword) {
+    public function SignUp(string $firstname, string $lastname, string $email, string $phone, string $password, string $confirmPassword)
+    {
         // if there's an owner session already, redirect to home
         $this->IfLoggedGoToIndex();
 
         if ($password != $confirmPassword) {
             Session::Set("error", "Passwords do not match");
             header("location:" . FRONT_ROOT . "Owner/SignUpView");
-			exit;
+            exit;
         }
-		
-		if ($this->ownerDAO->GetByEmail($email) != null) {
+
+        if ($this->ownerDAO->GetByEmail($email) != null) {
             Session::Set("error", "Email already exists");
             header("Location: " . FRONT_ROOT . "Owner/SignUpView");
-			exit;
+            exit;
         }
 
         $owner = new Owner();
@@ -48,7 +56,6 @@ class OwnerController {
         $owner->setPhone($phone);
         $owner->setPassword(password_hash($password, PASSWORD_DEFAULT));
 
-        
 
         $this->ownerDAO->Add($owner);
 
@@ -56,31 +63,35 @@ class OwnerController {
         header("location:" . FRONT_ROOT . "Owner");
     }
 
-    public function Login(string $email, string $password) {
+    public function Login(string $email, string $password)
+    {
         $owner = $this->ownerDAO->GetByEmail($email);
         if ($owner != null && password_verify($password, $owner->getPassword())) {
             Session::Set("owner", $owner);
             header("Location: " . FRONT_ROOT . "Owner");
-			exit;
+            exit;
         }
 
         Session::Set("error", "Invalid credentials");
         header("Location: " . FRONT_ROOT . "Owner/LoginView");
     }
 
-    public function LogOut() {
+    public function LogOut()
+    {
         Session::Logout();
         header("Location: " . FRONT_ROOT . "Home/Index");
     }
 
-    public function Pets() {
+    public function Pets()
+    {
         $this->VerifyIsLogged();
 
         $petList = $this->petDAO->GetPetsByOwnerId(Session::Get("owner")->getId());
         require_once(VIEWS_PATH . "list-pet.php");
     }
 
-    public function AddPet($name, $species, $breed, $age, $gender) {
+    public function AddPet($name, $species, $breed, $age, $gender)
+    {
         $this->VerifyIsLogged();
 
         $pet = new Pet();
@@ -95,52 +106,60 @@ class OwnerController {
 
     }
 
-    public function AddPetView() {
+    public function AddPetView()
+    {
         $this->VerifyIsLogged();
 
         require_once(VIEWS_PATH . "add-pet.php");
     }
 
-    public function EditPet(/* TODO: Parameters */) {
+    public function EditPet(/* TODO: Parameters */)
+    {
         $this->VerifyIsLogged();
 
         // TODO: Business logic
     }
 
-    public function RemovePet(/* TODO: Parameters */) {
+    public function RemovePet(/* TODO: Parameters */)
+    {
         $this->VerifyIsLogged();
-        
+
         // TODO: Business logic
     }
 
-    public function KeepersListView() {
+    public function KeepersListView()
+    {
         $this->VerifyIsLogged();
-        
-        // TODO: List keepers FR-6
+        $keeperList = $this->keeperDAO->GetAll();
+        require_once(VIEWS_PATH . "keeper-list.php");
     }
 
-    public function SignUpView() {
+    public function SignUpView()
+    {
         $this->IfLoggedGoToIndex();
         require_once(VIEWS_PATH . "owner-signup.php");
     }
 
-    public function LoginView() {
+    public function LoginView()
+    {
         $this->IfLoggedGoToIndex();
         require_once(VIEWS_PATH . "owner-login.php");
     }
 
-    private function IfLoggedGoToIndex() {
+    private function IfLoggedGoToIndex()
+    {
         if (Session::VerifySession("owner")) {
             header("Location: " . FRONT_ROOT . "Owner");
-			exit;
+            exit;
         }
     }
 
-    private function VerifyIsLogged() {
+    private function VerifyIsLogged()
+    {
         if (Session::VerifySession("owner") == false) {
             Session::Set("error", "You must be logged in to access this page.");
             header("location:" . FRONT_ROOT . "Owner/LoginView");
-			exit;
+            exit;
         }
     }
 }
