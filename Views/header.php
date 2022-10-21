c2c40f1539682efe36cad814d893f67519df809c
Matias Aceval <matiasacevalventura@gmail.com>
2022-10-21 00:17:44 -0300

    🔨 fix: login-style.css will now appear only to non logged users

--- Views/header.php
+++ Views/header.php
@@ -6,15 +6,21 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS 
-    <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH ?>bootstrap.min.css">-->
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH ?>bootstrap.min.css">
 
     <!-- Font Icon -->
     <link rel="stylesheet" href="<?php echo FONTS_PATH ?>material-icon/css/material-design-iconic-font.min.css">
 
-    <!-- Main css
-    <link rel="stylesheet" href="<?php echo CSS_PATH ?>style.css">
-    -->
+    <!-- Login & Signup css -->
+    <?php 
+        use Utils\Session;
+        if(!Session::VerifySession("owner") && !Session::VerifySession("keeper")){
+            ?>
+                <link rel="stylesheet" href="<?php echo CSS_PATH ?>login-style.css">
+            <?php
+        }
+    ?>   
 
     <!-- Favicon -->
     <link rel="shortcut icon" href="<?php echo VIEWS_PATH ?>img/favicon.ico" type="image/x-icon"/>
