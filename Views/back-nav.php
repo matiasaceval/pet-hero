bee028787cbfb58b2d19c8df7ff03a9f58495bab
Matias Aceval <matiasacevalventura@gmail.com>
2022-10-28 02:02:47 -0300

    ♻ update(view): back-btn adding logout and renaming home-nav

--- Views/back-nav.php
+++ Views/back-nav.php
@@ -1,3 +1,10 @@
 <nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-top">
-    <?php require_once(VIEWS_PATH."back-btn.php") ?>
+    <?php require_once(VIEWS_PATH . "back-btn.php") ?>
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <a class="nav-link nav-text" href="<?php echo FRONT_ROOT ?>Owner/Logout">
+                <button class="btn btn-primary" type="submit">Logout</button>
+            </a>
+        </li>
+    </ul>
 </nav>
