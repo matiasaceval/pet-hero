13cecd8779a1b61a2037a5f52b75af2f92277502
Matias Aceval <matiasacevalventura@gmail.com>
2022-10-31 20:59:47 -0300

    🆕 add(view): 404 not found

---
+++ Views/404.php
@@ -0,0 +1,30 @@
+<div class="glitch-bg">
+    <div class="container overflow-hidden">
+        <div class="centered-wrapper">
+            <div class="centered-element">
+                <div class="row justify-content-center">
+                    <div class="col-md-auto">
+                        <h1><span style="font-size: 128px">404</span></h1>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-md-auto">
+                        <h2><span style="font-size: 48px">Not Found</span></h2>
+                    </div>
+                </div>
+                <div class="row justify-content-center" style="margin-top: 32px">
+                    <div class="col-md-auto">
+                        <h3>the resource requested couldn't be found on the server :/</h3>
+                    </div>
+                </div>
+                <div class="row mt-5 justify-content-center">
+                    <div class="col-md-auto">
+                        <a href="<?php echo FRONT_ROOT ?>">
+                            <button class="btn btn-primary">Go to main page</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
