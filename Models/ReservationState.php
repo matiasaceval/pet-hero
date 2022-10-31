a2cf0fd7f26ef4a273ea66b171b8bd70468147dd
Maximo Torterolo Ambrosini <maximo.torterolo.a@gmail.com>
2022-10-31 17:35:49 -0300

    🆕 add(model): reservation state enum

--- Models/ReservationState.php
+++ Models/ReservationState.php
@@ -3,5 +3,11 @@
 namespace Models;
 
 class ReservationState {
-
+    const PENDING = "PENDING";
+    const ACCEPTED = "ACCEPTED";
+    const REJECTED = "REJECTED";
+    const CANCELED = "CANCELED";
+    const FINISHED = "FINISHED";
+    const PAID = "PAID";
+    const IN_PROGRESS = "IN_PROGRESS";
 }
