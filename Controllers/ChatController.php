a191f4021a17948d06f97850dad5b65a3b3b1fb3
Matias Aceval <matiasacevalventura@gmail.com>
2022-12-24 06:00:04 -0300

    🆕 add(controller): ChatController with temporal mockups

---
+++ Controllers/ChatController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Controllers;
+
+use Models\Chat;
+use Models\Keeper;
+use Models\Message;
+use Models\MessageState;
+use Models\Owner;
+use Utils\LoginMiddleware;
+use Utils\Session;
+use Utils\TempValues;
+
+class ChatController
+{
+
+    public function Index(string $id)
+    {
+        self::VerifyLogged();
+
+        $keeper = new Keeper();
+        $owner = new Owner();
+        $session = Session::Get("owner") ?? Session::Get("keeper");
+
+        // -------------- MOCKUP --------------
+        // here should be ChatDAO.getById($id), and if it's == null then redirect to 404
+        $chat = TempValues::GetValue("chat") ?? new Chat(1, $keeper, $owner, [
+            new Message($owner, $keeper, "Hi", "12/23/2022 22:15", "READ"),
+            new Message($keeper, $owner, "Hello", "12/23/2022 22:16", "READ"),
+            new Message($owner, $keeper, "How are you?", "12/23/2022 22:17", "READ"),
+            new Message($keeper, $owner, "I'm fine, and you?", "12/23/2022 22:18", "READ"),
+            new Message($owner, $keeper, "I'm fine too", "12/23/2022 22:19", "RECEIVED"),
+            new Message($owner, $keeper, "Can I ask you something?", "12/23/2022 22:20", "PENDING"),
+            new Message($owner, $keeper, "Hey", "12/23/2022 22:21", "PENDING"),
+            new Message($owner, $keeper, "Hey", "12/23/2022 22:22", "PENDING"),
+            new Message($owner, $keeper, "Hey", "12/23/2022 22:23", "PENDING"),
+            new Message($owner, $keeper, "Hey", "12/23/2022 22:32", "PENDING"),
+        ]);
+
+        if ($session instanceof Owner) {
+            $owner = $session;
+            $keeper->setId(3);
+            $keeper->setFirstname("John");
+            $keeper->setLastname("Doe");
+            $keeper->setEmail("asd@gmail.com");
+            $keeper->setPassword("1234");
+            $keeper->setPhone("123456789");
+            $keeper->setFee(1000);
+        } else if ($session instanceof Keeper) {
+            $keeper = $session;
+            $owner->setId(3);
+            $owner->setFirstname("Matías");
+            $owner->setLastname("Aceval");
+            $owner->setEmail("matiasaceval@gmail.com");
+            $owner->setPassword("1234");
+            $owner->setPhone("123456789");
+        }
+        // ------------ END OF MOCKUP ------------
+
+        $otherParticipant = $chat->getOtherParticipant($session);
+
+        TempValues::InitValues([
+            "chat" => $chat,
+            "session" => $session,
+            "other-participant" => $otherParticipant,
+            "back-page" => FRONT_ROOT . ($session instanceof \Models\Owner ? "Reservation/Reservations" : "Keeper/Reservations")
+        ]);
+
+        require_once(VIEWS_PATH . "chat.php");
+    }
+
+    public function SendMessage(string $chatId, string $message)
+    {
+        self::VerifyLogged();
+
+        $session = TempValues::GetValue("session");
+        $chat = TempValues::GetValue("chat");
+        $otherParticipant = TempValues::GetValue("other-participant");
+
+        $message = new Message($session, $otherParticipant, $message, date("m/d/Y H:i"), MessageState::PENDING);
+
+        // ChatDAO.sendMessage(Message)
+
+        // -------------- MOCKUP --------------
+        $chat->addMessage($message);
+        TempValues::InitValues(["chat" => $chat]);
+        // ------------ END OF MOCKUP ------------
+
+        header("Location: " . FRONT_ROOT . "Chat?id=" . $chatId);
+    }
+
+    private function VerifyLogged()
+    {
+        if (!LoginMiddleware::IsLogged()) {
+            header("Location: " . FRONT_ROOT . "Home/Index");
+            exit;
+        }
+    }
+}
---
+++ Views/chat.php
@@ -0,0 +1,74 @@
+<?php
+
+use Models\MessageState;
+
+require_once(VIEWS_PATH . "back-nav.php");
+?>
+<script>
+    $(document).ready(function() {
+        const chat = document.getElementsByClassName("card-body")[0];
+        chat.scrollTo({
+            top: $(".card-body")[0].scrollHeight,
+            behavior: 'instant'
+        });
+
+        $('form.message-form').submit(function(e) {
+            if ($('#message').val() == '') {
+                e.preventDefault();
+                return;
+            }
+        })
+    });
+</script>
+<div class="container">
+    <div class="card">
+        <div class="participant-info">
+            <p><?php echo $otherParticipant->getFullname(); ?></p>
+        </div>
+        <div class="card-body overflow-auto">
+
+            <!-- Mensajes aquí -->
+            <?php
+            $previousMessageFromOtherParticipant = false;
+            foreach ($chat->getMessages() as $row) {
+                echo "<div class='message'>";
+                if ($row->getSender() == $session) {
+                    // Muestra el mensaje alineado a la derecha
+                    echo "<div class='message text-right'>";
+                    echo "<p>" . $row->getText() . "</p>";
+                    echo "<div>";
+                    echo "<span class='time'>" . $row->getDate() . "</span>";
+                    if (
+                        $row->getState() == MessageState::READ
+                    ) {
+                        echo "<img width='24px' src='" . VIEWS_PATH . "/img/double-tick-blue.png' alt='Mensaje leído'>";
+                    } else if ($row->getState() == MessageState::RECEIVED) {
+                        echo "<img width='24px' src='" . VIEWS_PATH . "/img/double-tick-gray.png' alt='Mensaje recibido'>";
+                    } else {
+                        echo "<img width='24px' src='" . VIEWS_PATH . "/img/tick-gray.png' alt='Mensaje pendiente'>";
+                    }
+                    echo "</div>";
+                    echo "</div>";
+                    $previousMessageFromOtherParticipant = false;
+                } else {
+                    // Muestra el mensaje alineado a la izquierda
+                    echo "<div class='message text-left'>";
+                    if (!$previousMessageFromOtherParticipant) echo "<p class='chat-other-user'>" . $row->getSender()->getFirstName() . "</p>";
+                    echo "<p>" . $row->getText() . "</p>";
+                    echo "<span class='time'>" . $row->getDate() . "</span>";
+                    echo "</div>";
+                    $previousMessageFromOtherParticipant = true;
+                }
+                echo "</div>";
+            }
+            ?>
+        </div>
+        <div class="message-form-div">
+            <form class="message-form" action="<?php echo FRONT_ROOT ?>Chat/SendMessage" method="post">
+                <input type="hidden" name="chatId" value="<?php echo $chat->getId(); ?>">
+                <textarea type="text" name="message" id="message" placeholder="Escribe tu mensaje aquí" ocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false"></textarea>
+                <button class="message-send-button" type="submit"><img width='32px' src="<?php echo VIEWS_PATH ?>/img/send.png" alt="Enviar mensaje"></button>
+            </form>
+        </div>
+    </div>
+</div>
