dcfc202007fc01a603f8d4ddcaf08a483ed1fe6c
Matias Aceval <matiasacevalventura@gmail.com>
2022-12-26 22:27:55 -0300

    🆕 add(view): chat view

--- Views/chat.php
+++ Views/chat.php
@@ -5,19 +5,94 @@ use Models\MessageState;
 require_once(VIEWS_PATH . "back-nav.php");
 ?>
 <script>
+    let chat;
+
+    function refreshChat() {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", "<?php echo FRONT_ROOT . "Chat/Refresh?id=" . $chat->getId(); ?>", true);
+        xhr.onload = function() {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    let data = xhr.response;
+                    if (data != "") {
+                        console.log("Data fetched");
+                        data = data.split("</head>")[1];
+                        data = data.split("</body>")[0];
+                        data = data.replace(/\r/g, "");
+
+                        data = data.replace(/"/g, "'");
+                        let html = $(".card-body").html();
+                        html = html.replace(/"/g, "'");
+
+                        if (data != html) {
+                            console.log("Data changed");
+                            $(".card-body").html(data)
+                            chat.scrollTo({
+                                top: $(".card-body")[0].scrollHeight,
+                                behavior: 'smooth'
+                            });
+                        }
+                    }
+                }
+            }
+        }
+        xhr.send();
+    }
+
     $(document).ready(function() {
-        const chat = document.getElementsByClassName("card-body")[0];
+        chat = document.getElementsByClassName("card-body")[0];
         chat.scrollTo({
             top: $(".card-body")[0].scrollHeight,
             behavior: 'instant'
         });
 
+
         $('form.message-form').submit(function(e) {
-            if ($('#message').val() == '') {
-                e.preventDefault();
-                return;
+            e.preventDefault();
+        })
+
+        $('.message-form button').click(function(e) {
+            e.preventDefault();
+            let message = $('.message-form textarea').val();
+            if (/([^\s])/.test(message)) {
+                $('.card-body').append('<p class="loading"></p>');
+                let xhr = new XMLHttpRequest();
+                xhr.open("POST", "<?php echo FRONT_ROOT . "Chat/SendMessage" ?>", true);
+                xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                xhr.onload = function() {
+                    if (xhr.readyState === XMLHttpRequest.DONE) {
+                        if (xhr.status === 200) {
+                            let data = xhr.response;
+                            if (data != "") {
+                                console.log("Message sent");
+                                $('.loading').remove();
+                                chat.scrollTo({
+                                    top: $(".card-body")[0].scrollHeight,
+                                    behavior: 'smooth'
+                                });
+                            }
+                        }
+                    }
+                }
+                xhr.send("chatId=<?php echo $chat->getId(); ?>&message=" + message);
+                $('.message-form textarea').val("");
             }
         })
+
+        $(document).keydown(function(event) {
+            if (event.keyCode == 13 && !event.shiftKey) {
+                event.preventDefault();
+                $('.message-form button').click();
+            } else if (event.keyCode == 13 && event.shiftKey && $('.message-form textarea').val() != '') {
+                event.preventDefault();
+                $('.message-form textarea').val($('.message-form textarea').val() + "\r");
+            } else if (event.keyCode == 13 && event.shiftKey && $('.message-form textarea').val() == '') {
+                event.preventDefault();
+            }
+        });
+
+        refreshChat();
+        setInterval(refreshChat, 500);
     });
 </script>
 <div class="container">
@@ -26,38 +101,7 @@ require_once(VIEWS_PATH . "back-nav.php");
             <p><?php echo $otherParticipant->getFullname(); ?></p>
         </div>
         <div class="card-body overflow-auto">
-            <?php
-            $previousMessageFromOtherParticipant = false;
-            foreach ($chat->getMessages() as $row) {
-                echo "<div class='message'>";
-                if ($row->getSender() == $session) {
-                    echo "<div class='message text-right'>";
-                    echo "<p>" . $row->getText() . "</p>";
-                    echo "<div>";
-                    echo "<span class='time'>" . $row->getDate() . "</span>";
-                    if (
-                        $row->getState() == MessageState::READ
-                    ) {
-                        echo "<img width='24px' src='" . VIEWS_PATH . "/img/double-tick-blue.png' alt='Mensaje leído'>";
-                    } else if ($row->getState() == MessageState::RECEIVED) {
-                        echo "<img width='24px' src='" . VIEWS_PATH . "/img/double-tick-gray.png' alt='Mensaje recibido'>";
-                    } else {
-                        echo "<img width='24px' src='" . VIEWS_PATH . "/img/tick-gray.png' alt='Mensaje pendiente'>";
-                    }
-                    echo "</div>";
-                    echo "</div>";
-                    $previousMessageFromOtherParticipant = false;
-                } else {
-                    echo "<div class='message text-left'>";
-                    if (!$previousMessageFromOtherParticipant) echo "<p class='chat-other-user'>" . $row->getSender()->getFirstName() . "</p>";
-                    echo "<p>" . $row->getText() . "</p>";
-                    echo "<span class='time'>" . $row->getDate() . "</span>";
-                    echo "</div>";
-                    $previousMessageFromOtherParticipant = true;
-                }
-                echo "</div>";
-            }
-            ?>
+
         </div>
         <div class="message-form-div">
             <form class="message-form" action="<?php echo FRONT_ROOT ?>Chat/SendMessage" method="post">
